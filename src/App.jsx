d209d55086993aaf0803dcac2155cef4fa5d0f96
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-09-26 13:48:02 -0300

    sua fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Home from "./pages/Home/Index.jsx"
+import Header from './components/Header/Index.jsx'
+import Footer from './components/Footer/Index.jsx'
+import Slider from './components/swiper/Index.jsx'
+import About from './components/About/Index.jsx';
+
+import Section1 from './components/Section1/index.jsx';
+import Section2 from './components/Section2/index.jsx';
+import Section3 from './components/Section3/Index.jsx';
 
 export default function App() {
+    const slideContent = ['Texto do Slide 1', 'Texto do Slide 2', 'Texto do Slide 3'];
+    const slideCount = slideContent.length;
+
     return (
         <>
-            <Home />
+            <Header />
+            <main>
+                <Section1 />
+                <Section2 />
+                <Section3 />
+                <About />
+            </main>
+            <Footer />
         </>
     )
 }
--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -2,11 +2,13 @@
 export default function About(){
     return(
         <>
-            <section className="w-full h-full px-5 py-10 md:py-20 flex flex-col items-center">
-                <h2 className="font-bold uppercase text-[36px] text-center leading-8 mb-4 ">Medic <span className="font-bold uppercase text-[36px] text-[#009b55] ">Imagem Diagnósticos </span></h2>
-                <p className="text-center md:w-[70%] lg:w-[50%] font-normal">
-                    A CLÍNICA MEDIC IMAGEM DIAGNÓSTICOS é um Centro de Referência em Diagnósticos por Imagem em Patos de Minas e região desde 1999. Investindo em infraestrutura,  tecnologia e na formação médica e de seus  colaboradores, realizando exames convencionais e especializados de corpo inteiro.
-                </p>
+            <section className=" container mx-auto">
+                <div className="w-full h-full px-5 py-10 md:py-20 flex flex-col items-center">
+                    <h2 className="font-bold uppercase text-[36px] text-center leading-8 mb-4 ">Medic <span className="font-bold uppercase text-[36px] text-[#009b55] ">Imagem Diagnósticos </span></h2>
+                    <p className="text-center md:w-[70%] lg:w-[50%] font-normal">
+                        A CLÍNICA MEDIC IMAGEM DIAGNÓSTICOS é um Centro de Referência em Diagnósticos por Imagem em Patos de Minas e região desde 1999. Investindo em infraestrutura,  tecnologia e na formação médica e de seus  colaboradores, realizando exames convencionais e especializados de corpo inteiro.
+                    </p>
+                </div>
             </section>
         </>
     )
